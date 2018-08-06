b17f5404f59b0053bde3b13d063573276c62efa3
Usagi Ito <usagi@WonderRabbitProject.net>
2018-08-07 05:14:21 +0900

    add web_mercator and wgs84

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
+#![feature(extern_prelude)]
+
 pub mod math;
+pub mod wgs84;
+pub mod web_mercator;
 
 #[cfg(test)]
 #[test]
@@ -15,4 +19,19 @@ fn clamp()
   
   for value in MAX .. 2 * MAX
   { assert_eq!( math::clamp( value, MIN, MAX ), MAX ); }
+}
+
+#[cfg(test)]
+#[test]
+fn web_mercator()
+{
+  let ( lon0, lat0 ) = ( 43.062_083f64, 141.354_389f64 );
+  for lod in 0 .. 20
+  {
+    let angle_error_tolerance = 360.0f64 / ( 1u64 << lod ) as f64 / 256.0;
+    let ( px, py ) = web_mercator::angle_to_pixel( lon0, lat0, lod );
+    let ( lon1, lat1 ) = web_mercator::pixel_to_angle( px, py, lod );
+    assert!( lon1 - lon0 < angle_error_tolerance );
+    assert!( lat1 - lat0 < angle_error_tolerance );
+  }
 }
